72cce42e438c7ce3d59f124f75b774a33b769c9d
kennedh <ken.cmarcellino@gmail.com>
2026-04-21 10:57:45 -0300

    feat: add menu to modules

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
 from modules.hardware import get_hardware_info
+from modules.monitor import start_live_monitor
 
-data = get_hardware_info()
-for key, value in data.items():
-    print(f"{key.upper()}: {value}")
+
+def main():
+    print("1 - Relatório Completo de Hardware")
+    print("2 - Monitoramento em Tempo Real (Live)")
+
+    opcao = input("\nEscolha uma opção: ")
+
+    if opcao == '1':
+        info = get_hardware_info()
+        # lógica de print que você já fez...
+    elif opcao == '2':
+        start_live_monitor()
+    else:
+        print("Opção inválida.")
+
+
+if __name__ == "__main__":
+    main()
