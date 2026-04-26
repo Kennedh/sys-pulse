c3615f82b3c17c8cc6757462c1d47845602b96f8
kennedh <ken.cmarcellino@gmail.com>
2026-04-26 16:35:50 -0300

    feat: Atualizado para chamar a GUI

--- main.py
+++ main.py
@@ -1,20 +1,6 @@
-from modules.hardware import get_hardware_info
-from modules.monitor import start_live_monitor
-
-
-def main():
-    print("1 - Relatório Completo de Hardware")
-    print("2 - Monitoramento em Tempo Real (Live)")
-
-    opcao = input("\nEscolha uma opção: ")
-
-    if opcao == '1':
-        print(get_hardware_info())
-    elif opcao == '2':
-        start_live_monitor()
-    else:
-        print("Opção inválida.")
-
+from gui import App
 
 if __name__ == "__main__":
-    main()
+    # Instancia a interface e inicia o loop principal
+    app = App()
+    app.mainloop()
