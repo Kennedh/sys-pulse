f8c13479f2c1ad455dd256bf3d93033505612557
kennedh <ken.cmarcellino@gmail.com>
2026-05-27 14:14:00 -0300

    refactor: Tudo em Pyside QT 6 agora. Agora a main instancia o motor que gerencia as janelas da GUI

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
+import sys
+from PySide6.QtWidgets import QApplication
 from gui import App
 
 if __name__ == "__main__":
-    # Instancia a interface e inicia o loop principal
-    app = App()
-    app.mainloop()
+    # Instancia o motor do QT6 (Gerencia a interface e janelas)
+    app = QApplication(sys.argv)
+
+    # Chama a interface importada do gui
+    janela = App()
+
+    # Mostra a janela na tela (Meio obvio pela sintaxe mas vale o comentário)
+    janela.show()
+
+    # Inicia o loop do motor e garante a limpeza da memoria de maneira correta
+    sys.exit(app.exec())
